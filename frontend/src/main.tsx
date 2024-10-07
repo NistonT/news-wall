f60c17dee06a4b41df1861588f18f04bfef7463d
NistonN <nisprogrammer@gmail.com>
2024-10-07 05:48:39 +0300

    commit renderApp

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,29 @@
-import { QueryClient } from "@tanstack/react-query";
 import "./index.css";
-import { render } from "./render.tsx";
 
-const queryClient = new QueryClient();
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
+import { renderServer } from "./hooks/hooks.ts";
+import "./index.css";
+
+export const render = () => {
+	const queryClient = new QueryClient();
+
+	createRoot(document.getElementById("root")!).render(
+		<StrictMode>
+			<BrowserRouter>
+				<QueryClientProvider client={queryClient}>
+					<App />
+					<ReactQueryDevtools initialIsOpen={false} />
+				</QueryClientProvider>
+			</BrowserRouter>
+		</StrictMode>
+	);
+};
 
 render();
+
+renderServer(render);
